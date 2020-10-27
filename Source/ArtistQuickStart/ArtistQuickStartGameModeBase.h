a62de6389783a14d5f1af796323314d71ee62341
Van Phelan <van.phelan@aie.edu>
2020-10-27 16:57:15 -0500

    Created a floating object and imported Unity Chan
    for some reason

---
+++ Source/ArtistQuickStart/ArtistQuickStartGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArtistQuickStartGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARTISTQUICKSTART_API AArtistQuickStartGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
